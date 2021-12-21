53287ff2282e9b3de262ac05c815d3dc80926f4c
Ethic Hadebe <ethichadebe@gmail.com>
2021-12-21 10:32:11 +0200

    010 - Update: Added `MallItemDetailedView` activi

---
+++ app/src/main/java/com/example/chillersmarket/MallItemDetailedView.java
@@ -0,0 +1,14 @@
+package com.example.chillersmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MallItemDetailedView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mall_item_detailed_view);
+    }
+}
