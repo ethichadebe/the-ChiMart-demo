b0fb9491039883b24714792f8fd1710ff23e5ab4
freddie949 <freddiemokwena949>
2021-12-22 23:27:19 +0200

    Home page slider

--- app/src/main/java/com/example/chillersmarket/fragments/HomeFragment.java
+++ app/src/main/java/com/example/chillersmarket/fragments/HomeFragment.java
@@ -7,14 +7,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chillersmarket.Adapters.HomeAdapter;
 import com.example.chillersmarket.Adapters.MallAdapter;
+import com.example.chillersmarket.Adapters.SliderAdapter;
 import com.example.chillersmarket.R;
 import com.example.chillersmarket.RecyclewViewItems.HomeItem;
+import com.example.chillersmarket.RecyclewViewItems.HomeSlider;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 
@@ -25,6 +29,12 @@ public class HomeFragment extends Fragment {
     private RecyclerView.Adapter rvHomeItemsAdapter;
     private RecyclerView.LayoutManager rvHomeItemsLayoutManager;
     private ArrayList<HomeItem> homeItems = new ArrayList<>();
+
+    // Urls of our images.
+    String url1 = "https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
+    String url2 = "https://qphs.fs.quoracdn.net/main-qimg-8e203d34a6a56345f86f1a92570557ba.webp";
+    String url3 = "https://bizzbucket.co/wp-content/uploads/2020/08/Life-in-The-Metro-Blog-Title-22.png";
+
 
     @Nullable
     @Override
@@ -37,8 +47,41 @@ public class HomeFragment extends Fragment {
         homeItems.add(new HomeItem("Professionals/Practitioners", R.drawable.ic_professionals));
         homeItems.add(new HomeItem("Agriculture, Manufacturing, Food, Tourism", R.drawable.ic_food));
 
+        // we are creating array list for storing our image urls.
+        ArrayList<HomeSlider> sliderDataArrayList = new ArrayList<>();
+
+        // initializing the slider view.
+        SliderView sliderView = rootView.findViewById(R.id.slider);
+
+        // adding the urls inside array list
+        sliderDataArrayList.add(new HomeSlider(url1));
+        sliderDataArrayList.add(new HomeSlider(url2));
+        sliderDataArrayList.add(new HomeSlider(url3));
+
+        // passing this array list inside our adapter class.
+        SliderAdapter adapter = new SliderAdapter(getContext(), sliderDataArrayList);
+
+        // below method is used to set auto cycle direction in left to
+        // right direction you can change according to requirement.
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+
+        // below method is used to
+        // setadapter to sliderview.
+        sliderView.setSliderAdapter(adapter);
+
+        // below method is use to set
+        // scroll time in seconds.
+        sliderView.setScrollTimeInSec(3);
+
+        // to set it scrollable automatically
+        // we use below method.
+        sliderView.setAutoCycle(true);
+
+        // to start autocycle below method is used.
+        sliderView.startAutoCycle();
+
         //Setup RecyclerView display
-        rvHomeItems = view.findViewById(R.id.homeRecycleView);
+        rvHomeItems = view.findViewById(R.id.homeItemsRecycleView);
         rvHomeItems.setHasFixedSize(true);
         rvHomeItemsLayoutManager = new GridLayoutManager(getContext(), 2);
         rvHomeItemsAdapter = new HomeAdapter(getContext(), homeItems);
