12ede7d2365219016a6388debdf1eb324e2fdddd
Ethic Hadebe <ethichadebe@gmail.com>
2021-12-23 20:50:07 +0200

    013 - Update: mall Item click functionality

--- app/src/main/java/com/example/chillersmarket/Adapters/MallAdapter.java
+++ app/src/main/java/com/example/chillersmarket/Adapters/MallAdapter.java
@@ -25,7 +25,11 @@ public class MallAdapter extends RecyclerView.Adapter<MallAdapter.MallViewHolder
     private OnItemClickListener oiclListener;
 
     public interface OnItemClickListener{
-         void onItemClick(int position);
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        oiclListener = listener;
     }
 
 
@@ -34,13 +38,26 @@ public class MallAdapter extends RecyclerView.Adapter<MallAdapter.MallViewHolder
         public ImageView ivPreview;
         public TextView tvPrice, tvHeading, tvSeller;
 
-        public MallViewHolder(@NonNull View itemView) {
+        public MallViewHolder(@NonNull View itemView, OnItemClickListener listener) {
             super(itemView);
 
             ivPreview = itemView.findViewById(R.id.ivPreview);
             tvPrice = itemView.findViewById(R.id.tvPrice);
             tvHeading = itemView.findViewById(R.id.tvHeading);
             tvSeller = itemView.findViewById(R.id.tvSeller);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener!=null){
+                        int position = getAbsoluteAdapterPosition();
+
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 
@@ -53,7 +70,7 @@ public class MallAdapter extends RecyclerView.Adapter<MallAdapter.MallViewHolder
     @Override
     public MallViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.mall_item, parent, false);
-        MallViewHolder mvh = new MallViewHolder(v);
+        MallViewHolder mvh = new MallViewHolder(v, oiclListener     );
         return mvh;
     }
 
--- app/src/main/java/com/example/chillersmarket/fragments/ChillersMallFragment.java
+++ app/src/main/java/com/example/chillersmarket/fragments/ChillersMallFragment.java
@@ -1,5 +1,6 @@
 package com.example.chillersmarket.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chillersmarket.Adapters.MallAdapter;
+import com.example.chillersmarket.MallItemActivity;
 import com.example.chillersmarket.R;
 import com.example.chillersmarket.RecyclewViewItems.MallItem;
 
@@ -21,22 +23,32 @@ public class ChillersMallFragment extends Fragment {
 
     //Recycleview components
     private RecyclerView rvMallItems;
-    private RecyclerView.Adapter rvMallItemsAdapter;
+    private MallAdapter rvMallItemsAdapter;
     private RecyclerView.LayoutManager rvMallItemsLayoutManager;
     private ArrayList<MallItem> alMallItems = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_chillers_mall,container,false);
-        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/woocommerce-placeholder-300x300.png","R220.00","Floral upside down earrings","Sold By : nondumisomkhatshwa"));
-        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/12/ONDISPLAY2019flipbook-01-300x300.png","Free","Lihawu b/w earrings","Sold By : nondumisomkhatshwa"));
-        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/09/Chiller-Market-1-e1632735257569-300x266.png","R599.00","Medium","Sold By : Chillers Market"));
-        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/12/Flags_27-04-300x300.png","R250.00 Free","Orange 3D disk earrings","Sold By : nondumisomkhatshwa"));
-        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/12/pOP-uP-bANNER-05-300x300.jpg","R250.00","Rainbow/Floral headbands","Sold By : nondumisomkhatshwa"));
+        View v = inflater.inflate(R.layout.fragment_chillers_mall, container, false);
 
-        //Setup RecycleView display
         rvMallItems = v.findViewById(R.id.recycleView);
+
+        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/woocommerce-placeholder-300x300.png", "R220.00", "Floral upside down earrings", "Sold By : nondumisomkhatshwa"));
+        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/12/ONDISPLAY2019flipbook-01-300x300.png", "Free", "Lihawu b/w earrings", "Sold By : nondumisomkhatshwa"));
+        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/09/Chiller-Market-1-e1632735257569-300x266.png", "R599.00", "Medium", "Sold By : Chillers Market"));
+        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/12/Flags_27-04-300x300.png", "R250.00 Free", "Orange 3D disk earrings", "Sold By : nondumisomkhatshwa"));
+        alMallItems.add(new MallItem("https://chillersmarket.com/wp-content/uploads/2021/12/pOP-uP-bANNER-05-300x300.jpg", "R250.00", "Rainbow/Floral headbands", "Sold By : nondumisomkhatshwa"));
+
+        buildRecyclerView();
+
+        return v;
+    }
+
+    /**
+     * Setup RecycleView display
+     */
+    private void buildRecyclerView() {
         rvMallItems.setHasFixedSize(true);
         rvMallItemsLayoutManager = new GridLayoutManager(getContext(), 2);
         rvMallItemsAdapter = new MallAdapter(getContext(), alMallItems);
@@ -44,6 +56,11 @@ public class ChillersMallFragment extends Fragment {
         rvMallItems.setLayoutManager(rvMallItemsLayoutManager);
         rvMallItems.setAdapter(rvMallItemsAdapter);
 
-        return v;
+        rvMallItemsAdapter.setOnItemClickListener(new MallAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                startActivity(new Intent(getContext(), MallItemActivity.class));
+            }
+        });
     }
 }
