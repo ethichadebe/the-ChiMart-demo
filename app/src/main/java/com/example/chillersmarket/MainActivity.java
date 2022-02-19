0f6a4e8e474348a218a568c97944c2e4abaa9017
freddie949 <freddiemokwena949>
2022-02-19 23:47:54 +0200

    Account page details and click events

--- app/src/main/java/com/example/chillersmarket/MainActivity.java
+++ app/src/main/java/com/example/chillersmarket/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.chillersmarket;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,8 +7,10 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-import com.example.chillersmarket.fragments.ChillersMallFragment;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.chillersmarket.fragments.AccountFragment;
+import com.example.chillersmarket.fragments.ChillersMallFragment;
 import com.example.chillersmarket.fragments.EventsFragment;
 import com.example.chillersmarket.fragments.HomeFragment;
 import com.example.chillersmarket.fragments.ListingsFragment;
@@ -19,7 +19,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
     private TextView tvnCartItems;
 
-    public void setCartnItems(int nItems){
+    public void setCartnItems(int nItems) {
         tvnCartItems.setText(String.valueOf(nItems));
     }
 
@@ -31,12 +31,11 @@ public class MainActivity extends AppCompatActivity {
         tvnCartItems = findViewById(R.id.tvnCartItems);
 
         //Transparent Status bar
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        } else {
 
         }
-
 
         BottomNavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setOnItemSelectedListener(item -> {
--- app/src/main/java/com/example/chillersmarket/fragments/AccountFragment.java
+++ app/src/main/java/com/example/chillersmarket/fragments/AccountFragment.java
@@ -1,9 +1,12 @@
 package com.example.chillersmarket.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +16,65 @@ import com.example.chillersmarket.R;
 
 public class AccountFragment extends Fragment {
 
+    enum redirectToBrowser {
+        LISTING,
+        HELP
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account,container,false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        //Navigate to Edit Profile Fragment
+        toDifferentFragment(view.findViewById(R.id.txtEditProfile), new EditProfileFragment());
+
+        //Navigate to Change Password Fragment
+        toDifferentFragment(view.findViewById(R.id.txtChangePassword), new ChangePasswordFragment());
+
+        //Navigate to Order History Fragment
+        toDifferentFragment(view.findViewById(R.id.txtOrderHistory), new OrderHistoryFragment());
+
+        //Setup AddListing textview to redirect to a browser site
+        redirectToBrowserSetup(view.findViewById(R.id.txtAddListing), redirectToBrowser.LISTING);
+
+        //Setup Help textview to redirect to a browser site
+        redirectToBrowserSetup(view.findViewById(R.id.txtHelp), redirectToBrowser.HELP);
+
+        return view;
+    }
+
+    //Navigate to a different Fragment
+    private void toDifferentFragment(TextView textView, Fragment fragment){
+        textView.setOnClickListener(v -> {
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        });
+    }
+
+    //Navigate to browser page
+    private void redirectToBrowserSetup(TextView textView, redirectToBrowser redirect){
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = null;
+
+                switch (redirect){
+                    case LISTING:
+                        url = "https://chillersmarket.com/submit-listing/";
+                        break;
+                    case HELP:
+                        url = "https://chillersmarket.com/contact-us/";
+                        break;
+                    default:
+                        break;
+                }
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
